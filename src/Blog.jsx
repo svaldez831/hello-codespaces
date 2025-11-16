1fdde2b0d3451a815680554ce51c688998e1eeb5
svaldez831 <samuel.valdez@bsu.edu>
2025-11-16 09:14:23 +0000

    chore: added transtack to front end

---
+++ src/Blog.jsx
@@ -0,0 +1,30 @@
+import { CreatePost } from './components/CreatePost.jsx'
+import { PostList } from './components/PostList.jsx'
+import { PostFilter } from './components/PostFilter.jsx'
+import { PostSorting } from './components/PostSorting.jsx'
+
+const posts = [
+  {
+    title: 'Full-Stack React Projects',
+    contents: "Let's become full-stack developers!",
+    author: 'Daniel Bugl',
+  },
+  { title: 'Hello React!' },
+]
+export function Blog() {
+  return (
+    <div style={{ padding: 8 }}>
+      <CreatePost />
+      <br />
+      <hr />
+      Filter by:
+      <PostFilter field='author' />
+      <br />
+      <PostSorting fields={['createdAt', 'updatedAt']} />
+      <hr />
+      <PostList posts={posts} />
+    </div>
+  )
+}
+
+export default Blog

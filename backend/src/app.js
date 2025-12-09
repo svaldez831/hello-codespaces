e8ca82373a1e155df2f9e0a8f766c6b93a7e4b2f
svaldez831 <samuel.valdez@bsu.edu>
2025-12-09 07:14:50 +0000

    feat: changed from crud to graphql query

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,42 @@
 import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4'
+
+import { typeDefs, resolvers } from './graphql/index.js'
 import { postsRoutes } from './routes/posts.js'
 import { userRoutes } from './routes/users.js'
-import bodyParser from 'body-parser'
-import cors from 'cors'
 import { eventRoutes } from './routes/events.js'
+import { optionalAuth } from './middleware/jwt.js'
+
 const app = express()
-app.use(bodyParser.json())
 app.use(cors())
+app.use(bodyParser.json())
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+apolloServer.start().then(() =>
+  app.use(
+    '/graphql',
+    optionalAuth,
+    expressMiddleware(apolloServer, {
+      context: async ({ req }) => {
+        return { auth: req.auth }
+      },
+    }),
+  ),
+)
 
 postsRoutes(app)
 userRoutes(app)
 eventRoutes(app)
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express LIVES sdfasdfsa !')
+  res.send('Hello World from Express!')
 })
 
 export { app }
